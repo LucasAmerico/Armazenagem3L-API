5578cc78052d8bbdf08c25e0ec28ce17c2d18e78
Lucas Alcântara <lucasalcantara18@gmail.com>
2020-11-25 11:50:15 -0300

    estrutura de pastas

---
+++ Armazenagem3L-API/Repositories/impl/CargaRepositoryImpl.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Armazenagem3L_API.Repositories.impl {
+    public class CargaRepositoryImpl : CargaRepository{
+    }
+}
