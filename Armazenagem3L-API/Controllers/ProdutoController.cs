63a5a7b2cf9841122da5a905c7465be0484268be
Lucas Simão <lucas.caixeta@dtidigital.com.br>
2020-11-26 10:49:08 -0300

    #2 feat(service): metodo para listagem de produtos

---
+++ Armazenagem3L-API/Controllers/ProdutoController.cs
@@ -0,0 +1,28 @@
+﻿using Armazenagem3L_API.Models;
+using Armazenagem3L_API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Armazenagem3L_API.Controllers
+{
+    [Route("api/produto")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private ProdutoService service = new ProdutoService();
+
+        // GET api/produto/listagem?id=5
+        [HttpGet("listagem")]
+        public IActionResult Listagem(int id)
+        {
+            List<Produto> lista = service.listagemProdutos(id);
+            return Ok(lista);
+        }
+
+    }
+}
---
+++ Armazenagem3L-API/Services/ProdutoService.cs
@@ -0,0 +1,43 @@
+﻿using Armazenagem3L_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Armazenagem3L_API.Services
+{
+    public class ProdutoService
+    {
+        private List<Produto> produtos = new List<Produto>() { 
+            new Produto()
+            {
+                Id = 1,
+                Nome = "Laranja",
+                Peso = 20,
+                Preco = 10
+            },
+            new Produto()
+            {
+                Id = 2,
+                Nome = "Maça",
+                Peso = 50,
+                Preco = 25
+            },
+            new Produto()
+            {
+                Id = 3,
+                Nome = "Abacaxi",
+                Peso = 30,
+                Preco = 15
+            }
+    };
+    public List<Produto> listagemProdutos(int id) {
+
+            if (id != null) { 
+                //TODO Acessar o repository para buscar o produto especifico informado
+            }
+
+            return produtos;
+        }
+    }
+}
